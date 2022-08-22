1060e8127b741430776c5596f988fbb9557c6961
cantillo28 <103455831+cantillo28@users.noreply.github.com>
2022-08-22 17:13:05 -0300

    Add files via upload

---
+++ projeto.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Meu projeto</h1>
+
+    <img src="{{ asset('img/messi.png')}}">
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
